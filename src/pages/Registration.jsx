12e20a4ebbbc657bb62389f34b893958485e4a37
fihadranzer <rhfiha@gmail.com>
2023-09-17 17:44:20 +0600

    CIT chat Registration with Validation Part completed

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, TextField, Button } from "@mui/material";
 import "../styles/registration.css";
 const Registration = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [passwordLengthError, setPasswordLengthError] = useState("");
+  const [matchedPassword, setMatchedPassword] = useState("");
+
+  const handleOnSubmit = () => {
+    if (!name) {
+      setNameError("please enter fullname");
+    } else if (!email) {
+      setEmailError("Please Enter your email");
+      setNameError("");
+    } else if (!password) {
+      setPasswordError("Please Enter your password ");
+      setEmailError("");
+    } else if (password.length < 8) {
+      setPasswordLengthError("Password minimum 8 character in length ");
+      setPasswordError("");
+    } else if (!confirmPassword) {
+      setConfirmPasswordError("Please confirm your password");
+      setPasswordLengthError("");
+    } else if (password !== confirmPassword) {
+      setMatchedPassword("Password not matched");
+      setConfirmPasswordError("");
+    } else {
+      setMatchedPassword("");
+    }
+  };
+
+  console.log(name);
   return (
     <section className="registration__part">
       <Grid container spacing={0}>
@@ -11,38 +46,56 @@ const Registration = () => {
               <h1>Get started with easily register</h1>
               <p>Free register and you can enjoy it</p>
               <TextField
-                helperText=""
+                helperText={nameError}
                 id="demo-helper-text-misaligned"
                 label="Full Name"
                 style={{ width: "360px", marginTop: "30px" }}
                 type="text"
+                onChange={(e) => setName(e.target.value)}
               />
               <br />
               <TextField
-                helperText=""
+                helperText={emailError}
                 id="demo-helper-text-misaligned"
                 label="Email"
                 style={{ width: "360px", marginTop: "30px" }}
                 type="email"
-              />{" "}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <br />
               <TextField
-                helperText=""
+                helperText={
+                  passwordError
+                    ? passwordError
+                    : passwordLengthError
+                    ? passwordLengthError
+                    : ""
+                }
                 id="demo-helper-text-misaligned"
                 label="Password"
                 style={{ width: "360px", marginTop: "30px" }}
                 type="password"
-              />{" "}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <br />
               <TextField
-                helperText=""
+                helperText={
+                  confirmPasswordError
+                    ? confirmPasswordError
+                    : matchedPassword
+                    ? matchedPassword
+                    : ""
+                }
                 id="demo-helper-text-misaligned"
                 label="Confirm Password"
                 style={{ width: "360px", marginTop: "30px" }}
                 type="password"
-              />{" "}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
               <br />
-              <Button variant="contained">Sign up</Button>
+              <Button variant="contained" onClick={handleOnSubmit}>
+                Sign up
+              </Button>
             </div>
           </div>
         </Grid>
